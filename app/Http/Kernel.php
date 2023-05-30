58c879851499b62570bafb2c2a8ee8b3ff5ac748
MD. Rezaul Islam <rezaul.cse.mbstu@gmail.com>
2023-05-30 14:55:42 +0600

    📦 chore(Kernel.php): add Fruitcake\Cors\HandleCors middleware to the middleware stack 📦 chore(composer.json): add fruitcake/laravel-cors package to the project dependencies 📦 chore(composer.json): add kylekatarnls/update-helper plugin to allow automatic updates of composer dependencies 📦 chore(config/cors.php): add default CORS configuration file The Fruitcake\Cors\HandleCors middleware is added to the middleware stack to handle CORS requests. The fruitcake/laravel-cors package is added to the project dependencies to provide the middleware. The kylekatarnls/update-helper plugin is added to allow automatic updates of composer dependencies. A default CORS configuration file is added to the config directory.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -12,6 +12,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
+        \Fruitcake\Cors\HandleCors::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \App\Http\Middleware\EncryptCookies::class,
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,

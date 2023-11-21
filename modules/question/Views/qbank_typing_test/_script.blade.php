5df7c04f94326ee5bc871a3fb9d74ed9c2cb23ac
MD. Rezaul Islam <rezaul.cse.mbstu@gmail.com>
2023-11-21 20:17:22 +0600

    character count

--- modules/question/Views/qbank_typing_test/_script.blade.php
+++ modules/question/Views/qbank_typing_test/_script.blade.php
@@ -1,35 +1,35 @@
 <script>
-
-$(document).ready(function() {
-
-
-
-	
-
-	// console.log($('.typing_question').val());
-
-	// $('#words_count').val(words_count);
-
-	$('body').on('keyup','.typing_question',function(event) {
-	   
-	var words_count = $(this).val().trim().split(/\s+/).length;
-
-	if ($(this).val().trim() =='') {
-
-		words_count =0;
-	}
-
-	$(this).closest('.repeater-block').find('.words-count').val(words_count);
-
-	$(this).closest('.typing-test-modal').find('.words-count').val(words_count);
-
-	});
+    $(document).ready(function() {
 
 
 
-	$('.typing_question').trigger('keyup');
-
-});
 
 
+        // console.log($('.typing_question').val());
+
+        // $('#words_count').val(words_count);
+
+        $('body').on('keyup', '.typing_question', function(event) {
+
+            //var words_count = $(this).val().trim().split(/\s+/).length;
+            var character_count = $(this).val().trim().length;
+
+            if ($(this).val().trim() == '') {
+                //words_count = 0;
+                character_count = 0;
+            }
+
+            //$(this).closest('.repeater-block').find('.words-count').val(words_count);
+            $(this).closest('.repeater-block').find('.words-count').val(character_count);
+
+            //$(this).closest('.typing-test-modal').find('.words-count').val(words_count);
+            $(this).closest('.typing-test-modal').find('.words-count').val(character_count);
+
+        });
+
+
+
+        $('.typing_question').trigger('keyup');
+
+    });
 </script>
